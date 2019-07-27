023fbf92e286afebbc654733a360856d1a8211de
LeSaiyan <bastien.pinard@ynov.com>
2019-07-27 11:55:16 +0000

    Add files via upload

---
+++ ProjectDevOps/samplephpwebsite-1/testFunction.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class testFunction extends TestCase
+{
+    function TestsiteName()
+{
+    $this->('name')
+    echo config('name');
+}
+function TestsiteVersion()
+{
+    $this->('version')
+    echo config('version');
+}
+
+function >TestnavMenu($sep = ' | ')
+{
+
+}
+
+
+}
